4d63b57bf0eef013d27fb3deadec188b1589ac07
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-11-21 12:35:08 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './index.css';
+import './css/style.css';
 
 function App() {
   return (
-    <main>
+    <main className='midle' >
       <div className="container">
+        <div className="card">
       <h3>0 birthdays today</h3>
-      <button className='btn'>Clear All</button>
+      <button className='btn btn-primary'>Clear All</button>
+      </div>
       </div>
     </main>
   );
